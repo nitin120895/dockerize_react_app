0eb656b504851081cc71e948eb4128ffcce0585a
nitin120895 <61227133+nitin120895@users.noreply.github.com>
2024-11-08 17:11:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with nitin test
+          Learn React with nitin test new pull
         </a>
       </header>
     </div>
