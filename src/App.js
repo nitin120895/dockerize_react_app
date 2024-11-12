c0b768189bbe4d16368d3e6187eed8b509158878
nitin120895 <61227133+nitin120895@users.noreply.github.com>
2024-11-12 12:12:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 
+          Learn React  nn
         </a>
       </header>
     </div>
