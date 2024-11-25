d737cfbf88178e04e47b605525e0d5e02a8fdde6
nitin120895 <61227133+nitin120895@users.noreply.github.com>
2024-11-25 18:09:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 
+          Learn React patch 47
         </a>
       </header>
     </div>
